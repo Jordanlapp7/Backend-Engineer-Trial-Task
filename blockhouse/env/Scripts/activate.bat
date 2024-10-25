9ecf6b6c28106965f20de5bba8fd34d066d30099
Jordan Lapp <jordanlapp7@gmail.com>
2024-10-24 20:42:23 -0400

    Added env vars for database

--- blockhouse/env/Scripts/activate.bat
+++ blockhouse/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jorda\OneDrive\Desktop\Projects\Blockhouse Backend Engineer Trial Task\blockhouse\env
+set VIRTUAL_ENV=C:\Users\jorda\OneDrive\Desktop\Projects\Jordan Lapp Blockhouse Backend Engineer Trial Task\blockhouse\env
 
 if not defined PROMPT set PROMPT=$P$G
 
